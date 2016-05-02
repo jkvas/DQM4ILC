1801104d4c03e81f2a271a249fbc25ec49817888
airqui <adrian.irles@desy.de>
2016-05-02 21:49:02 +0200

    Examples before testbeam may 2016

--- source/examples/AHCALRawModule/jobinit.sh
+++ source/examples/AHCALRawModule/jobinit.sh
@@ -2,4 +2,5 @@ source /home/calice/Programs/ilcinstall-v01-17-09/v01-17-09/init_ilcsoft.sh
 export LD_LIBRARY_PATH=/home/calice/Programs/DQM4HEP-AHCAL/lib:$LD_LIBRARY_PATH
 export PATH=/home/calice/Programs/DQM4HEP-AHCAL/bin:$PATH
 export DIM_DNS_NODE=localhost
-export DQM4HEP_PLUGIN_DLL=/home/calice/Programs/DQM4HEP-AHCAL/lib/libDQM4ILC.so:/home/calice/Programs/DQM4HEP-AHCAL/lib/libAHCALRawModule.so
+
+./home/calice/Programs/DQM4HEP-AHCAL/bin/dqm4hep_start_job_interface
--- source/examples/AHCALRawModule/run_local.sh
+++ source/examples/AHCALRawModule/run_local.sh
@@ -4,11 +4,13 @@
 #
 # works in all steps except for the analysis module.
 # !!
-source /space/airqui/Programs/DQM4HEP/ilcinstall/v01-17-07/init_ilcsoft.sh
-export LD_LIBRARY_PATH=/space/airqui/Programs/DQM4HEP/lib:$LD_LIBRARY_PATH
-export PATH=/space/airqui/Programs/DQM4HEP/bin:$PATH
 
+source /home/calice/Programs/ilcinstall-v01-17-09/v01-17-09/init_ilcsoft.sh
+export LD_LIBRARY_PATH=/home/calice/Programs/DQM4HEP-AHCAL/lib:$LD_LIBRARY_PATH
+export PATH=/home/calice/Programs/DQM4HEP-AHCAL/bin:$PATH
 export DIM_DNS_NODE=localhost
+
+
 
 dns &
 #sudo 
@@ -17,12 +19,14 @@ dns &
 # and all the settings should be set again (LD_LIBRARY_PATH, etc)
 dimjc_daemon &
 
-export DQM4HEP_PLUGIN_DLL=/space/airqui/Programs/DQM4HEP/lib/libDQM4ILC.so
+export DQM4HEP_PLUGIN_DLL=/home/calice/Programs/DQM4HEP-AHCAL/lib/libDQM4ILC.so
 dqm4hep_start_event_collector -c ahcal_event_collector -s LCIOStreamer &
 
 dqm4hep_start_monitor_element_collector -c ahcal_me_collector &
 
 dqm4hep_start_lcio_file_service -f /space/airqui/Programs/Run_30270__23p08p2015__12p29p10.slcio -c ahcal_event_collector -r -t 100000 &
+
+dqm4hep_start_lcio_file_service -f /home/calice/TestFile_ChannelbyChannel.slcio -c ahcal_event_collector -r -t 100000 &
 
 #The analysis module fails, it does not complains about the AHCALRawModule, but about other stuff...
 # if I run the randomodule in examples, I see the same errors.
@@ -30,7 +34,12 @@ dqm4hep_start_lcio_file_service -f /space/airqui/Programs/Run_30270__23p08p2015_
 # Wrong communication with the streamer ?
 
 export DQM4HEP_PLUGIN_DLL=/space/airqui/Programs/DQM4HEP/lib/libDQM4ILC.so/space/airqui/Programs/DQM4HEP/lib/libAHCALRawModule.so
+
+export DQM4HEP_PLUGIN_DLL=/home/calice/Programs/DQM4HEP-AHCAL/lib/libDQM4ILC.so:/home/calice/Programs/DQM4HEP-AHCAL/lib/libAHCALRawModule.so
+
 dqm4hep_start_analysis_module -f /space/airqui/Programs/DQM4HEP/DQM4ILC/source/examples/AHCALRawModule/lcioAHCALTests.xml
+
+dqm4hep_start_analysis_module -f /home/calice/Programs/DQM4HEP-AHCAL/DQM4ILC/source/examples/AHCALRawModule/lcioAHCALTests.xml
 
 
 
