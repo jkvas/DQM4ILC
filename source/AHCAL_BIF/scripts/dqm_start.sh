ee946cc3ee6e52eb72155bb5e5fbfe07a91927c8
airqui <adrian.irles@desy.de>
2018-03-28 20:43:31 +0200

    scripts added

---
+++ source/AHCAL_BIF/scripts/dqm_start.sh
@@ -0,0 +1,42 @@
+
+# Starting the individual processes
+dns &
+#source dns_script.sh &
+
+dqm4hep_start_run_control_server -v ALL -r AHCALRunControl & 
+
+dqm4hep_start_run_control_interface -v ALL -r AHCALRunControl 
+#--settings-file Runcontrol_Detector_center.xml &
+
+export DQM4HEP_PLUGIN_DLL=/home/calice/TB2018/DQM4ILC/lib/libAHCAL_40Layers.so
+dqm4hep_start_event_collector -v ALL -c ahcal_event_collector &
+
+dqm4hep_start_monitor_element_collector -v ALL -c ahcal_me_collector &
+
+
+dqm4hep_start_dqm_monitor_gui -v ALL
+
+
+
+
+#dqm4hep_start_run_control_interface -v WARN -r AHCALRunControl --settings-file Runcontrol_Detector_corner.xml &
+
+#dqm4hep_start_dqm_monitor_gui -v WARN --import-file DQM_Monitoring_canvases.xml &
+#--import-file DQM_2017July.xml& 
+#dqm4hep_start_dqm_monitor_gui -v WARN &
+
+sleep 2;
+
+dqm4hep_start_analysis_module -v ALL -f /home/calice/TB2018/scripts/lcioTestbeam201805.xml &
+
+#start the analysis modules
+#dqm4hep_start_analysis_module -v WARN -f ../DQM4HEP/DQM4ILC/source/AHCAL_BIF/AHCAL/Layers/lcioTestbeam201707.xml&
+#sleep 5
+#dqm4hep_start_analysis_module -v WARN -f ~/TB2017/DQM4HEP/DQM4ILC/source/AHCAL_BIF/AHCAL/Channels/lcio15channelsX4.xml&
+
+#dqm4hep_start_analysis_module -v WARN -f ~/TB2017/DQM4HEP/DQM4ILC/source/AHCAL_EUDETtelescope/GeomCorrelations/lcioTelescopeCorrelations.xml&
+
+
+#dqm4hep_start_lcio_file_service -f /home/calice/TestFile_ChannelbyChannel.slcio -c ahcal_event_collector -r -t 10
+#dqm4hep_start_analysis_module -f /home/calice/Programs/DQM4HEP-AHCAL/DQM4ILC/source/examples/AHCALRawModule/lcioAHCALTests.xml
+ 
---
+++ source/AHCAL_BIF/scripts/generate_geometry.sh
@@ -0,0 +1,132 @@
+#!/bin/sh
+LAYERS=40
+LayerOrder=(1 #module No. of 1st layer
+    2 #module No. of 2nd layer ...
+    3 
+    4
+    5
+    6
+    7
+    8
+    9
+    10
+    11
+    12
+    13
+    14
+    15
+    16
+    17
+    18
+    19
+    20 
+    21 
+    22
+    23
+    24
+    25
+    26
+    27
+    28
+    29
+    30 
+    31
+    32
+    33
+    34
+    35
+    36
+    37
+    38
+    39
+    40
+)
+ModuleFirstAsics=(
+    0 #1st asic# of module 1
+    0 #1st asic# of module 2
+    0 #1st asic# of module 3
+    0 #1st asic# of module 4
+    0 #1st asic# of module 5
+    0 #1st asic# of module 6
+    0 #1st asic# of module 7
+    0 #1st asic# of module 8
+    0 #1st asic# of module 9
+    0 #1st asic# of module 10
+    0 #1st asic# of module 11
+    0 #1st asic# of module 12
+    0 #1st asic# of module 13
+    0 #1st asic# of module 14
+    0 #1st asic# of module 15
+    0 #1st asic# of module 16
+    0 #1st asic# of module 17
+    0 #1st asic# of module 18
+    0 #1st asic# of module 19
+    0 #1st asic# of module 20
+    0 #1st asic# of module 21
+    0 #1st asic# of module 22
+    0 #1st asic# of module 23
+    0 #1st asic# of module 24
+    0 #1st asic# of module 25
+    0 #1st asic# of module 26
+    0 #1st asic# of module 27
+    0 #1st asic# of module 28
+    0 #1st asic# of module 29
+    0 #1st asic# of module 30
+    0 #1st asic# of module 31
+    0 #1st asic# of module 32
+    0 #1st asic# of module 33
+    0 #1st asic# of module 34
+    0 #1st asic# of module 35
+    0 #1st asic# of module 36
+    0 #1st asic# of module 37
+    0 #1st asic# of module 38
+    0 #1st asic# of module 39
+    0 #1st asic# of module 40
+)
+
+I_OFFSET=1 ; #where the I starts from
+J_OFFSET=1 
+K_OFFSET=1
+for layer in `seq 0 $((LAYERS-1))` 
+do
+    module=${LayerOrder[layer]}
+    echo "  <layer id=\"${layer}\" module=\"${module}\">"
+    for asic in `seq 0 15`
+    do
+	chipid=$(( ModuleFirstAsics[LayerOrder[layer] - 1] + asic ))
+	echo "    <chip id=\"$((chipid + (256 * module) ))\" index=\"${asic}\">"
+	case $asic in 
+	    0|1|4|5) ASICX=18 ;;
+	    2|3|6|7) ASICX=12 ;;
+	    8|9|12|13) ASICX=6 ;;
+	    10|11|14|15) ASICX=0 ;;
+	esac;
+	case $asic in 
+	    0|8)ASICY=18;;
+	    1|9)ASICY=12;;
+	    2|10)ASICY=23;;
+	    3|11)ASICY=17;;
+	    4|12)ASICY=6;;
+	    5|13)ASICY=0;;
+	    6|14)ASICY=11;;
+	    7|15)ASICY=5;;
+	esac;
+	case $asic in 
+	    0|1|4|5|8|9|12|13)ASICYDIRECTION=1;;
+	    2|3|6|7|10|11|14|15)ASICYDIRECTION=-1;;
+	esac;
+
+	for channel in `seq 0 35`
+	do
+	    xdiv=$((channel / 6))
+	    xmod=$((channel % 6))
+	    COOR_I=$((ASICX + ( channel / 6 ) + I_OFFSET ))
+	    COOR_J=$((ASICY + ( ASICYDIRECTION * ( channel % 6 ) ) + J_OFFSET))
+	    echo "      <channel id=\"${channel}\" j=\"${COOR_J}\" i=\"${COOR_I}\" k=\"$((layer + K_OFFSET))\"/>"
+	   # echo "(${asic},${channel}) $COOR_I $COOR_J $xdiv $xmod"
+	done ; #channel
+	echo "    </chip>"
+    done ; #asic
+    echo "  </layer>"
+    echo "" ; #empty new line
+done ; #layer
